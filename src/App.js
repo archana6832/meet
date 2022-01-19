f61494e0bf20b4862caf14a43b99dda7a2885d57
archana6832 <57583300+archana6832@users.noreply.github.com>
2022-01-19 11:03:51 +0100

    welcome screen removed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
-import WelcomeScreen from './WelcomeScreen';
-import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
+//import WelcomeScreen from './WelcomeScreen';
+//import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
+import { getEvents, extractLocations } from './api';
 import NumberOfEvents from './NumberOfEvents';
 import './nprogress.css';
 import { WarningAlert } from './Alert';
@@ -14,7 +15,7 @@ class App extends Component {
   state = {
     events: [], //to pass events state to EventList component
     locations: [], // to pass locations state to citysearch component
-    showWelcomeScreen: undefined,
+    // showWelcomeScreen: undefined,
     numberOfEvents: 32,
     currentLocation: 'all',
     errorText: ''
@@ -47,7 +48,15 @@ class App extends Component {
   }
 
   //componentDidMount
-  async componentDidMount() {
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+  /*async componentDidMount() {
     this.mounted = true;
     const accessToken = localStorage.getItem('access_token');
     const isTokenValid = (await checkToken(accessToken)).error ? false :
@@ -62,24 +71,26 @@ class App extends Component {
         }
       });
     }
-  }
+  }*/
   // componentWillUnmount
   componentWillUnmount() {
     this.mounted = false;
   }
+
+
   render() {
-    if (this.state.showWelcomeScreen === undefined)
-      return (
-        <div className="App">
-          {!navigator.onLine ? (<WarningAlert text='You are offline!' />) : (<WarningAlert text='' />)}
-          <p className="meet">MEET APP</p>
-          <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-          <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateNumberOfEvents={this.updateNumberOfEvents} />
-          <EventList events={this.state.events} />
-          <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
-            getAccessToken={() => { getAccessToken() }} />
-        </div>
-      );
+    //if (this.state.showWelcomeScreen === undefined) return <div className="App" />
+    return (
+      <div className="App">
+        {!navigator.onLine ? (<WarningAlert text='You are offline!' />) : (<WarningAlert text='' />)}
+        <p className="meet">MEET APP</p>
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateNumberOfEvents={this.updateNumberOfEvents} />
+        <EventList events={this.state.events} />
+        {/* <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => { getAccessToken() }} />*/}
+      </div>
+    );
   }
 }
 
